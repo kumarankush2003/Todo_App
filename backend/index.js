df9fa2f1fb5773474a218283b19dd94151c9f2fc
kumarankush2003 <ankushkumar642003@gmail.com>
2025-03-22 13:43:06 +0530

    second

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,28 @@
 const express = require("express")
 const app =express();
 app.use(express.json());
+const {createTodo} =require("./type")
 
 app.post("/todo",function(req,res){
-
+    const createpayload=req.body;
+    const ParsePayload=createTodo.safeParse(createpayload);
+    if(!ParsePayload.success){
+        res.status(411).json({
+            message:"Invalid payload",
+        })
+        return;
+        }
 })
 app.get("/todos",function(req,res){
     
 })
 app.put("/completed",function(req,res){
-    
+    const UpdatePayload= req.body;
+    const ParsePayload=updateTodo.safeParse(UpdatePayload);
+    if(!ParsePayload.success){
+        res.status(411).json({
+            message:"Invalid payload",
+            })
+            return;
+            }
 })
